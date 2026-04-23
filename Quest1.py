238e8facfde062b57546a9e18f1065bf8bb3882e
DG0202 <d11180506@gmail.com>
2026-04-23 11:30:54 -0300

    Q1 até Q3: Resolvidas

---
+++ Quest1.py
@@ -0,0 +1,7 @@
+variavel = 1
+print('comeco')
+while variavel <= 10:
+    print('variavel = ', variavel)
+    variavel = variavel + 1
+
+print('fim')
---
+++ Questao2.py
@@ -0,0 +1,4 @@
+variavel = 10
+while variavel >= 1:
+    print('variavel =', variavel)
+    variavel = variavel - 1
