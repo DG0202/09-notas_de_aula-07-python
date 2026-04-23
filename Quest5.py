145523a2368fd0c8be74d36ca4c99e25ffedae1a
DG0202 <d11180506@gmail.com>
2026-04-23 11:49:00 -0300

    4 a 5: Resolvidas

---
+++ Quest5.py
@@ -0,0 +1,5 @@
+numb = 5
+while numb <= 50:
+    
+    print('multiplos de 5 =', numb)
+    numb += 5
--- Questao3.py
+++ Questao3.py
@@ -1,1 +1,4 @@
 numero = 2
+while numero <= 20:
+    print('numero par =', numero)
+    numero = numero + 2
